71feb000a7e72a89103b3863e46b534fe7d3456f
Jake Stone <jstone14@student.fitchburgstate.edu>
2021-10-18 12:28:37 -0400

    forgot to stage and push my final changes last night, my apologies

--- Jake_Stone_Mobile_Game_Design/Assets/Scripts/AppendageAttacher.cs
+++ Jake_Stone_Mobile_Game_Design/Assets/Scripts/AppendageAttacher.cs
@@ -10,31 +10,62 @@ public class AppendageAttacher : MonoBehaviour
     public List<Rigidbody2D> skrinkle = new List<Rigidbody2D>(); //list of dino pieces to be added to spine until completion 
     private int spinos = 0; // sets the amount of spinos attached to '0'
     public int spinosb4APP; //the amount of spinos to be added to the green spine before you can start adding other pieces
+    //bool fired;
+    public Touch tuuc;
 
-    void Update() //I'd like to thank Christian Hotte for the programming assistance
+    void Start()
     {
-        if (Input.GetButtonDown("Jump")) //this wacky 'if' statement takes in player input, fires a dino piece onto the rotating spine, & adds said piece to a list in the inspector
+        //fired = false;
+    }
+
+    private void Update() //big thanks to Christian Hotte for the programming assistance
+    {
+        Attachpendage();
+        //StartCoroutine(WaitForNextShot);
+        //yield return new WaitForSeconds(1);
+        //StopCoroutine(WaitForNextShot);
+    }
+
+    void Attachpendage()
+    {
+        if (Input.touchCount > 0) //this wacky 'if' statement takes in player input, fires a dino piece onto the rotating spine, & adds said piece to a list in the inspector
         {
-            Rigidbody2D pieceShoooter;
-            if (spinos >= spinosb4APP)
-            {   
-                if (skrinkle.Count == 0)
-                {
-                    return;
-                }
-                pieceShoooter = Instantiate(skrinkle[0], pos.position, pos.rotation) as Rigidbody2D;
-                skrinkle.RemoveAt(0);
-            }
-            else
+            tuuc = Input.GetTouch(0);
+
+            if(tuuc.phase == TouchPhase.Began)
             {
-                pieceShoooter = Instantiate(appendage, pos.position, pos.rotation) as Rigidbody2D;
-                spinos++;
+                Rigidbody2D pieceShoooter;
+                if (spinos >= spinosb4APP)
+                {
+                    if (skrinkle.Count == 0)
+                    {
+                        return;
+                    }
+                    pieceShoooter = Instantiate(skrinkle[0], pos.position, pos.rotation) as Rigidbody2D;
+                    skrinkle.RemoveAt(0);
+                }
+                else
+                {
+                    pieceShoooter = Instantiate(appendage, pos.position, pos.rotation) as Rigidbody2D;
+                    spinos++;
+                }
+                pieceShoooter.AddForce(pos.position * -200f);
+                bunkus.Add(pieceShoooter); //makes list of projectiles
+                //fired = true;
             }
-            pieceShoooter.AddForce(pos.position * -200f);
-            bunkus.Add(pieceShoooter); //makes list of projectiles
+
+
         }
 
-
-
     }
+
+    //IEnumerator WaitForNextShot(float time2Wait)
+    //{
+    //    time2Wait = 1f;
+    //    while (fired == true)
+    //    {
+    //        yield return null;
+    //    }
+    //}
+
 }
